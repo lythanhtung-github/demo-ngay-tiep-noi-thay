d56b653cc12fc74bc24c486cf11a8b829aed95ae
Ly Thanh Tung <lt_tung>
2026-03-12 16:37:58 +0700

    add image

---
+++ assets/js/wish-data.js
@@ -0,0 +1,18 @@
+const wishList = [
+    { 
+        name: `Nước Lọc`,
+        content: `🪷 Hành trình kết nối những nhân duyên thật đặc biệt. Gia đình nhỏ chúng con thấy thật may mắn, biết ơn khi có Thầy luôn đồng hành, yểm trợ và chứng minh cho chúng con trong những bước chuyển quan trọng của cuộc đời. Nhìn lại vẫn thấy quá phước đức, quá mầu nhiệm. Nhân ngày tiếp nối Thầy, chúng con kính chúc Thầy sẽ tiếp tục gieo nhiều nhân duyên lành cho nhiều thế hệ tiếp nối. Thầy vẫn luôn là điểm tựa vững tin cho chúng con khi nghĩ về một người Thầy luôn quên mình mà nghĩ cho tất cả.... 🪷 Biết ơn Thầy của chúng con.`
+    },
+    { 
+        name: `Nhật Uyên`,
+        content: `Kính chúc Thầy ngày tiếp nối an lạc, vạn sự cát tường. Cảm ơn Thầy đã luôn dẫn dắt chúng con trên con đường đạo hạnh.`
+    },
+    { 
+        name: `Thanh Tùng`,
+        content: ``
+    },
+    { 
+        name: `Học trò 3`,
+        content: `...`
+    },
+];
